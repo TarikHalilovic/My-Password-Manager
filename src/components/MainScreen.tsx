799e1ae9c63109b2654ef0fc36303970c67dcca3
TarikHalilovic <tarik.halilovic@protonmail.ch>
2021-04-22 02:44:10 +0200

    Change names of async functions

--- src/components/MainScreen.tsx
+++ src/components/MainScreen.tsx
@@ -1,120 +1,138 @@
-import React, { useState, useEffect } from 'react';
-import { SearchBar } from 'react-native-elements';
-import { StyleSheet, View } from "react-native";
-import { MyTable } from './MyTable';
+import React, {useState, useEffect} from 'react';
+import {SearchBar} from 'react-native-elements';
+import {StyleSheet, View} from 'react-native';
+import {MyTable} from './MyTable';
 import Icon from 'react-native-vector-icons/FontAwesome';
 
-import { MMKVService } from '../service/MMKVService';
+import {MMKVService} from '../service/MMKVService';
 
 export const MainScreen = ({navigation, route}) => {
-  const [storageService, setStorageService] = useState(null);
-  const [data, setData] = useState([]);
-  const [visibleData, setVisibleData] = useState([]);
+    const [storageService, setStorageService] = useState(null);
+    const [data, setData] = useState([]);
+    const [visibleData, setVisibleData] = useState([]);
 
-  useEffect(() => {
-    const service = MMKVService.initialize();
-    setStorageService(service);
-    MMKVService.getAll(service).then((result) => {
-      setData(result);
-      setVisibleData(result);
-    });
-  }, []);
+    useEffect(() => {
+        const service = MMKVService.initialize();
+        setStorageService(service);
+        MMKVService.getAllAsync(service).then(result => {
+            setData(result);
+            setVisibleData(result);
+        });
+    }, []);
 
-  const [lastEntryInAction, setLastEntryInAction] = useState(null);
-  let entry: {id: string; name:string; username: string; email: string; pw: string} = route.params?.entry;  
+    const [lastEntryInAction, setLastEntryInAction] = useState(null);
+    let entry: {
+        id: string;
+        name: string;
+        username: string;
+        email: string;
+        pw: string;
+    } = route.params?.entry;
 
-  const [searchText, setSearchText] = useState("");
+    const [searchText, setSearchText] = useState('');
 
-  function onChangeSearch(textValue: string){
-    setSearchText(textValue);
-    textValue = textValue.toLowerCase();
-    if(textValue == ""){
-      setVisibleData(data);
+    function onChangeSearch(textValue: string) {
+        setSearchText(textValue);
+        textValue = textValue.toLowerCase();
+        if (textValue == '') {
+            setVisibleData(data);
+        } else {
+            setVisibleData(
+                data.filter(
+                    x =>
+                        x.name.toLowerCase().includes(textValue) ||
+                        x.username.toLowerCase().includes(textValue),
+                ),
+            );
+        }
     }
-    else{
-      setVisibleData(
-        data.filter(
-          x => x.name.toLowerCase().includes(textValue) || 
-          x.username.toLowerCase().includes(textValue)
-        )
-      );
+
+    function deleteEntry(rowId: string) {
+        MMKVService.removeAsync(storageService, rowId);
+
+        setData(data.filter(x => x.id != rowId));
+        let searchTextLower = searchText.toLowerCase();
+        setVisibleData(
+            data.filter(
+                x =>
+                    x.id != rowId &&
+                    (x.name.includes(searchTextLower) ||
+                        x.username.includes(searchTextLower)),
+            ),
+        );
     }
-  }
 
-  function deleteEntry(rowId: string){
-    MMKVService.remove(storageService, rowId);
+    function addEditEntry(entry: {
+        id: string;
+        name: string;
+        username: string;
+        email: string;
+        pw: string;
+    }) {
+        let newData: any;
+        if (entry.id == '0') {
+            let newId: string = null;
+            MMKVService.addAsync(storageService, entry).then(id => {
+                newId = id;
+            });
+            entry.id = newId;
 
-    setData(data.filter(x => x.id != rowId));
-    let searchTextLower = searchText.toLowerCase();
-    setVisibleData(data.filter(x =>
-      x.id != rowId &&
-      (x.name.includes(searchTextLower) || 
-      x.username.includes(searchTextLower))
-    ));
-  }
-
-  function addEditEntry(entry: {id: string; name:string; username: string; email: string; pw: string}){
-    let newData: any;
-    if(entry.id == "0"){
-      let newId: string = null;
-      MMKVService.add(storageService, entry).then(id => {
-        newId = id;
-      });
-      entry.id = newId;
-
-      newData = [entry, ...data];
-      setData(newData);
+            newData = [entry, ...data];
+            setData(newData);
+        } else {
+            MMKVService.edit(storageService, entry);
+            newData = [entry, ...data.filter(x => x.id != entry.id)];
+            setData(newData);
+        }
+        setVisibleData(newData);
+        setSearchText('');
     }
-    else{
-      MMKVService.edit(storageService, entry);
-      newData = [entry, ...data.filter(x => x.id != entry.id)];
-      setData(newData);
+
+    if (
+        entry != undefined &&
+        (lastEntryInAction == null ||
+            entry.id != lastEntryInAction.id ||
+            entry.name != lastEntryInAction.name ||
+            entry.username != lastEntryInAction.username ||
+            entry.email != lastEntryInAction.email ||
+            entry.pw != lastEntryInAction.pw)
+    ) {
+        addEditEntry(entry);
+        setLastEntryInAction(entry);
     }
-    setVisibleData(newData);
-    setSearchText("");
-  }
 
-  if(entry != undefined && (lastEntryInAction == null ||
-    (entry.id != lastEntryInAction.id ||
-    entry.name != lastEntryInAction.name ||
-    entry.username != lastEntryInAction.username ||
-    entry.email != lastEntryInAction.email ||
-    entry.pw != lastEntryInAction.pw))){
-      addEditEntry(entry);
-      setLastEntryInAction(entry);
-}
-
-  return (
-    <View style={styles.container}>
-      <SearchBar
-        placeholder="Search"
-        onChangeText={onChangeSearch}
-        value={searchText}
-      />
-      <Icon.Button 
-        name="plus"
-        backgroundColor="#23C552"
-        onPress={() => navigation.navigate('AddEditEntry')}>
-        Add
-      </Icon.Button>
-      <View style={styles.wrapperAroundMyTable}>
-        <MyTable
-          rows={visibleData}
-          deleteAction={deleteEntry}
-          navigation={navigation}
-        />
-      </View>
-    </View>
-  );
-}
+    return (
+        <View style={styles.container}>
+            <SearchBar
+                placeholder="Search"
+                onChangeText={onChangeSearch}
+                value={searchText}
+            />
+            <Icon.Button
+                name="plus"
+                backgroundColor="#23C552"
+                onPress={() => navigation.navigate('AddEditEntry')}
+            >
+                Add
+            </Icon.Button>
+            <View style={styles.wrapperAroundMyTable}>
+                <MyTable
+                    rows={visibleData}
+                    deleteAction={deleteEntry}
+                    navigation={navigation}
+                />
+            </View>
+        </View>
+    );
+};
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'black',
-  },
-  wrapperAroundMyTable:{
-    flex: 1,
-    marginTop: 6,
-  }
+    container: {
+        flex: 1,
+        backgroundColor: 'black',
+    },
+    wrapperAroundMyTable: {
+        flex: 1,
+        marginTop: 6,
+    },
 });
--- src/service/MMKVService.tsx
+++ src/service/MMKVService.tsx
@@ -1,82 +1,81 @@
 // https://rnmmkv.vercel.app/#/callbackapi
 
-import MMKVStorage from "react-native-mmkv-storage";
+import MMKVStorage from 'react-native-mmkv-storage';
 
 export const MMKVService = {
     initialize,
-    getAll,
-    add,
-    remove,
+    getAllAsync,
+    addAsync,
+    removeAsync,
     edit,
 };
 
-function initialize(){
+function initialize() {
     return new MMKVStorage.Loader()
-        .withInstanceID('testInstance4')
+        .withInstanceID('mymkkvinstance-1')
         .setProcessingMode(MMKVStorage.MODES.MULTI_PROCESS)
         .withEncryption()
         .initialize();
 }
 
-async function getAll(storage: MMKVStorage.API){
+async function getAllAsync(storage: MMKVStorage.API) {
     let data: Array<object> = [];
 
-    let keys = await storage.getArrayAsync("allEntryKeys");
-    if(keys != null){
-        keys.forEach(async (key) => {
+    let keys = await storage.getArrayAsync('allEntryKeys');
+    if (keys != null) {
+        keys.forEach(async key => {
+            // TODO: GetMultiple doesnt work for now add fix when fixed
             let item = await storage.getMapAsync(key);
-            if(item != null) data.push(item);
+            if (item != null) data.push(item);
         });
         return data;
     }
-    
+
     return [];
 }
 
-function edit(storage: MMKVStorage.API, entry: {id: string}){
+function edit(storage: MMKVStorage.API, entry: {id: string}) {
     storage.setMap(entry.id, entry);
 }
 
-async function add(storage: MMKVStorage.API, entry: {id: string}){
+async function addAsync(storage: MMKVStorage.API, entry: {id: string}) {
     let cleanKey: string;
-    
-    const key = await storage.getIntAsync("nextId");
-    if(key != null){
+
+    const key = await storage.getIntAsync('nextId');
+    if (key != null) {
         cleanKey = key.toString();
-        storage.setInt("nextId", key+1);
-    }
-    else{
-        cleanKey = "1";
-        storage.setInt("nextId", 2);
+        storage.setInt('nextId', key + 1);
+    } else {
+        cleanKey = '1';
+        storage.setInt('nextId', 2);
     }
     entry.id = cleanKey;
     storage.setMap(cleanKey, entry);
 
-    storage.getArray("allEntryKeys", (err: string, result: Array<string>) => {
-        if(err) return;
-        if(result != null){
-            storage.setArray("allEntryKeys", [...result, cleanKey]);
-        }
-        else{
-            storage.setArray("allEntryKeys", [cleanKey]);
+    storage.getArray('allEntryKeys', (err: string, result: Array<string>) => {
+        if (err) return;
+        if (result != null) {
+            storage.setArray('allEntryKeys', [...result, cleanKey]);
+        } else {
+            storage.setArray('allEntryKeys', [cleanKey]);
         }
     });
-    
+
     return cleanKey;
 }
 
-async function remove(storage: MMKVStorage.API, id: string){
-    const allKeys = await storage.getArrayAsync("allEntryKeys");
+async function removeAsync(storage: MMKVStorage.API, id: string) {
+    const allKeys = await storage.getArrayAsync('allEntryKeys');
 
-    if(allKeys != null){
+    if (allKeys != null) {
         let newKeys: Array<string> = [];
         allKeys.forEach(key => {
-            if(key != id){
+            if (key != id) {
                 newKeys.push(key);
             }
-        })
-        await storage.setArrayAsync("allEntryKeys", newKeys);
+        });
+        await storage.setArrayAsync('allEntryKeys', newKeys);
     }
 
-    return await storage.removeItem(id); // Returning null?
+    return await storage.removeItem(id); // Always returns null?
 }
