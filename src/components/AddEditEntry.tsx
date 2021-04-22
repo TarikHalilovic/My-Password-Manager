be92d2c0a59aee24cbea156c0d09e72042f92af4
TarikHalilovic <tarik.halilovic@protonmail.ch>
2021-04-22 02:34:36 +0200

    Change colors on input and button

--- src/components/AddEditEntry.tsx
+++ src/components/AddEditEntry.tsx
@@ -1,82 +1,130 @@
-import React, { useState } from 'react';
-import { StyleSheet, View, Text, TextInput, Button } from "react-native";
+import React, {useState} from 'react';
+import {StyleSheet, View, Text, TextInput, Button} from 'react-native';
 
 export const AddEditEntry = ({navigation, route}) => {
     const [nameError, setNameError] = useState('');
     const [entry, setEntry] = useState(
-        (route.params?.row != undefined) ?
-        route.params.row :
-        {
-            id: "0",
-            name: '',
-            username: '',
-            email: '',
-            pw: '',
-        }
+        route.params?.row != undefined
+            ? route.params.row
+            : {
+                  id: '0',
+                  name: '',
+                  username: '',
+                  email: '',
+                  pw: '',
+              },
     );
 
-    function validate(){
-        if(entry.name == ""){
-            setNameError("Service name/url must have value.")
+    function validate() {
+        if (entry.name == '') {
+            setNameError('Service name/url must have value.');
             return false;
-        }
-        else{
+        } else {
             return true;
         }
     }
 
-    function generateRandomPw(pwLength: number = 16){
+    function generateRandomPw(pwLength: number = 16) {
         // Not truly random
-        return Array(pwLength).fill("0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!@-#$").map(function(x) { return x[Math.floor(Math.random() * x.length)] }).join('');;
+        return Array(pwLength)
+            .fill(
+                '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!@-#$',
+            )
+            .map(function (x) {
+                return x[Math.floor(Math.random() * x.length)];
+            })
+            .join('');
     }
-    
+
     return (
         <View style={styles.container}>
             <View>
                 <Text style={styles.formLabel}>
-                    {(entry.id == "0" ? 'New' : 'Edit')} Entry
+                    {entry.id == '0' ? 'New' : 'Edit'} Entry
                 </Text>
             </View>
             <View>
                 <TextInput
                     placeholder="Service name/url"
+                    placeholderTextColor="#2f3831"
                     style={styles.inputStyle}
                     value={entry.name}
-                    onChangeText={(val) => setEntry({id: entry.id, name:val, username: entry.username, email: entry.email, pw: entry.pw})}
+                    onChangeText={val =>
+                        setEntry({
+                            id: entry.id,
+                            name: val,
+                            username: entry.username,
+                            email: entry.email,
+                            pw: entry.pw,
+                        })
+                    }
                 />
                 <TextInput
                     placeholder="Username"
+                    placeholderTextColor="#2f3831"
                     style={styles.inputStyle}
                     value={entry.username}
-                    onChangeText={(val) => setEntry({id: entry.id, name: entry.name, username: val, email: entry.email, pw: entry.pw})}
+                    onChangeText={val =>
+                        setEntry({
+                            id: entry.id,
+                            name: entry.name,
+                            username: val,
+                            email: entry.email,
+                            pw: entry.pw,
+                        })
+                    }
                 />
                 <TextInput
                     placeholder="Email"
+                    placeholderTextColor="#2f3831"
                     style={styles.inputStyle}
                     value={entry.email}
-                    onChangeText={(val) => setEntry({id: entry.id, name: entry.name, username: entry.username, email: val, pw: entry.pw})}
+                    onChangeText={val =>
+                        setEntry({
+                            id: entry.id,
+                            name: entry.name,
+                            username: entry.username,
+                            email: val,
+                            pw: entry.pw,
+                        })
+                    }
                 />
                 <TextInput
                     placeholder="Password"
+                    placeholderTextColor="#2f3831"
                     style={styles.inputStyle}
                     value={entry.pw}
-                    onChangeText={(val) => setEntry({id: entry.id, name: entry.name, username: entry.username, email: entry.email, pw: val})}
+                    onChangeText={val =>
+                        setEntry({
+                            id: entry.id,
+                            name: entry.name,
+                            username: entry.username,
+                            email: entry.email,
+                            pw: val,
+                        })
+                    }
                 />
                 <View style={styles.errorContainer}>
                     <Text style={styles.errorText}>{nameError}</Text>
                 </View>
-                <Button 
+                <Button
                     title="Generate Password"
                     onPress={() => {
-                        setEntry({id: entry.id, name: entry.name, username: entry.username, email: entry.email, pw: generateRandomPw()});
+                        setEntry({
+                            id: entry.id,
+                            name: entry.name,
+                            username: entry.username,
+                            email: entry.email,
+                            pw: generateRandomPw(),
+                        });
                     }}
-                    color="#bad1f5"
+                    color="#0225c2"
                 />
                 <View style={styles.submitButtonWrapper}>
-                    <Button 
+                    <Button
                         title="Submit"
                         onPress={() => {
-                            if(validate()){
+                            if (validate()) {
                                 navigation.navigate('MainScreen', {entry});
                             }
                         }}
@@ -85,15 +133,15 @@ export const AddEditEntry = ({navigation, route}) => {
             </View>
         </View>
     );
-}
+};
 
 const styles = StyleSheet.create({
     container: {
-      flex: 1,
-      backgroundColor: 'black',
-      alignItems: 'center',
-      justifyContent: 'center',
-      height: 50,
+        flex: 1,
+        backgroundColor: 'black',
+        alignItems: 'center',
+        justifyContent: 'center',
+        height: 50,
     },
     formLabel: {
         fontSize: 17,
@@ -114,9 +162,9 @@ const styles = StyleSheet.create({
     errorContainer: {
         alignItems: 'center',
         justifyContent: 'center',
-        marginBottom: 7
+        marginBottom: 7,
     },
-    submitButtonWrapper:{
+    submitButtonWrapper: {
         marginTop: 10,
-    }
-  });
+    },
+});
